55fe553b16b70d52ea33435160f89dc814df6815
Dylan McMullen <dmcmull1@my.centennialcollege.ca>
2024-11-28 10:47:03 -0500

    adding db access

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // Simulate database access
+            Console.WriteLine("Database access has already been done on Nov 28, 2024, by Dylan.");
+
+            // Optionally log this message using the ILogger
+            _logger.LogInformation("Database access has been logged on Nov 28, 2024, by Dylan.");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
